d284d403f9af9c27200351825b5e1fef05c13365
harshuk17 <harshdhiman8410@gmail.com>
2025-11-22 16:35:54 +0530

    chat implementation built

--- app/api/mobile/chat/route.ts
+++ app/api/mobile/chat/route.ts
@@ -1,50 +1,50 @@
-import { authMiddleware } from "@/middleware";
-import { PrismaClient } from "@prisma/client";
-import type { JwtPayload } from "jsonwebtoken";
-import { NextRequest, NextResponse } from "next/server";
+  import { authMiddleware } from "@/middleware";
+  import { PrismaClient } from "@prisma/client";
+  import type { JwtPayload } from "jsonwebtoken";
+  import { NextRequest, NextResponse } from "next/server";
 
-import prisma from "@/lib/prisma";
+  import prisma from "@/lib/prisma";
 
-export async function GET(request: NextRequest) {
-  try {
-    const decoded = (await authMiddleware(request)) as JwtPayload & {
-      id: string;
-    };
+  export async function GET(request: NextRequest) {
+    try {
+      const decoded = (await authMiddleware(request)) as JwtPayload & {
+        id: string;
+      };
 
-    const chats = await prisma.chat.findMany({
-      where: {
-        OR: [{ userId: decoded.id }, { trainerId: decoded.id }],
-      },
-      include: {
-        user: {
-          select: {
-            name: true,
-            image: true,
+      const chats = await prisma.chat.findMany({
+        where: {
+          OR: [{ userId: decoded.id }, { trainerId: decoded.id }],
+        },
+        include: {
+          user: {
+            select: {
+              name: true,
+              image: true,
+            },
+          },
+          trainer: {
+            select: {
+              name: true,
+              image: true,
+            },
+          },
+          messages: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            take: 1,
           },
         },
-        trainer: {
-          select: {
-            name: true,
-            image: true,
-          },
-        },
-        messages: {
-          orderBy: {
-            createdAt: "desc",
-          },
-          take: 1,
-        },
-      },
-    });
+      });
 
-    return NextResponse.json({ chats });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        error:
-          error instanceof Error ? error.message : "An unknown error occurred",
-      },
-      { status: 500 }
-    );
+      return NextResponse.json({ chats });
+    } catch (error) {
+      return NextResponse.json(
+        {
+          error:
+            error instanceof Error ? error.message : "An unknown error occurred",
+        },
+        { status: 500 }
+      );
+    }
   }
-}
